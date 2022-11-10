c4d1c1ab1d9a2f8425e1926a34fc9da4ab7dd059
HippolyteMith <hippolyte.mithouard@gmail.com>
2022-11-10 09:47:42 +0100

    (feat) Ajout InscriptionDtoHandler et modif afin de l'implemente

--- gamelife/src/main/java/fr/sqli/formation/gamelife/dto/InscriptionDtoHandler.java
+++ gamelife/src/main/java/fr/sqli/formation/gamelife/dto/InscriptionDtoHandler.java
@@ -1,23 +1,39 @@
 package fr.sqli.formation.gamelife.dto;
 
-import fr.sqli.formation.gamelife.entity.ProduitEntity;
 
-public class ProduitDtoHandler {
+import fr.sqli.formation.gamelife.entity.UtilisateurEntity;
 
-	public static ProduitDto fromEntity(ProduitEntity entity) {
-		var resu = new ProduitDto();
-		resu.setCategorie(entity.getCategorie());
-		resu.setNom(entity.getNom());
-		resu.setDetail(entity.getDetail());
-		resu.setPlateforme(entity.getPlateforme());
-		resu.setPrix(entity.getPrix());
-		resu.setId(entity.getId());
-		return resu;
+public class InscriptionDtoHandler {
+
+	public static InscriptionDto fromEntity(InscriptionDto entity) {
+		var u = new InscriptionDto();
+		u.setPrenom(entity.getPrenom());
+		u.setNom(entity.getNom());
+		u.setMdp(entity.getMdp());
+		u.setEmail(entity.getEmail());
+		u.setVille(entity.getVille());
+		u.setRue(entity.getRue());
+		u.setNum_rue(entity.getNum_rue());
+		u.setNum_siren(entity.getNum_siren());
+		u.setEtat(entity.getEtat());
+		u.setRole(entity.getRole());
+		return u;
 	}
 
-	public static ProduitEntity fromDto(ProduitDto dto) {
-		// Inchallah un jour faut coder
-		return null;
+	public static UtilisateurEntity fromDto(InscriptionDto dto) {
+
+		var u = new UtilisateurEntity();
+		u.setPrenom(dto.getPrenom());
+		u.setNom(dto.getNom());
+		u.setMdp(dto.getMdp());
+		u.setEmail(dto.getEmail());
+		u.setVille(dto.getVille());
+		u.setRue(dto.getRue());
+		u.setNumRue(dto.getNum_rue());
+		u.setNumSiren(dto.getNum_siren());
+		u.setEtatCompte(dto.getEtat());
+		u.setRole(dto.getRole());
+		return u;
 	}
 
 }
