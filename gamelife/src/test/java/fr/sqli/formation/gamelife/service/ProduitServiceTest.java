4345384bd772b10522b9ca997fc5a5716a593b86
Fabien BIDAULT <fabien.bidault@social.aston-ecole.com>
2022-11-15 09:40:39 +0100

    (fix) remplacer jeux par jeu, et changement au niveau de la méthode isPresent, par size pour retourner la taille du tableau

--- gamelife/src/test/java/fr/sqli/formation/gamelife/service/ProduitServiceTest.java
+++ gamelife/src/test/java/fr/sqli/formation/gamelife/service/ProduitServiceTest.java
@@ -1,10 +1,16 @@
 package fr.sqli.formation.gamelife.service;
 
 import fr.sqli.formation.gamelife.entity.ProduitEntity;
+import fr.sqli.formation.gamelife.ex.ProduitException;
+import fr.sqli.formation.gamelife.repository.ProduitRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class ProduitServiceTest {
@@ -23,10 +29,6 @@ public class ProduitServiceTest {
         }
     }
 
-    /**
-     * Attention: La base de données est bien case insensitive
-     * @throws Exception
-     */
     @Test
     void testRechercherJeuxVideo02() throws Exception {
         String jeuvideo = "Call";
@@ -36,5 +38,17 @@ public class ProduitServiceTest {
         for (ProduitEntity j : jeux) {
             Assertions.assertTrue(j.getNom().toLowerCase().startsWith(jeuvideo.toLowerCase()));
         }
+    }
+
+    @Test
+    void testRechercheJeuxVideo03() {
+        String jeuvideo = "";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> service.getProductsByName(jeuvideo));
+    }
+
+    @Test
+    void testRechercheJeuxVideo04() {
+        String jeuvideo = "%f";
+        Assertions.assertThrows(ProduitException.class, () -> service.getProductsByName(jeuvideo));
     }
 }
