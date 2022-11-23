0a817d0ff4a8592a6b0614e40c1483c5e8915879
Quentin Bossuet <qbossuet@gmail.com>
2022-11-23 15:25:23 +0100

    (refactor) ajout de condition dans les méthodes de GestionCompteService

--- gamelife/src/main/java/fr/sqli/formation/gamelife/service/GestionCompteService.java
+++ gamelife/src/main/java/fr/sqli/formation/gamelife/service/GestionCompteService.java
@@ -4,6 +4,7 @@ import fr.sqli.formation.gamelife.dto.GestionCompteDto;
 import fr.sqli.formation.gamelife.dto.GestionEtatDto;
 import fr.sqli.formation.gamelife.dto.GestionMdpDto;
 import fr.sqli.formation.gamelife.entity.UtilisateurEntity;
+import fr.sqli.formation.gamelife.ex.OldPasswordException;
 import fr.sqli.formation.gamelife.ex.UtilisateurExistantException;
 import fr.sqli.formation.gamelife.repository.UtilisateurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,16 +20,39 @@ public class GestionCompteService {
 
     public UtilisateurEntity modificationCompte(GestionCompteDto dto) throws Exception{
         var user = uDao.findById(dto.getId());
+        var control = uDao.findByEmail(dto.getEmail());
         if(user.isPresent()){
             UtilisateurEntity u = uDao.findById(dto.getId()).get();
-            u.setPrenom(dto.getPrenom());
-            u.setNom(dto.getNom());
-            u.setEmail(dto.getEmail());
-            u.setNumRue(dto.getNumRue());
-            u.setRue(dto.getRue());
-            u.setVille(dto.getVille());
-            u.setCodePostal(dto.getCodePostal());
-            u.setNumSiren(dto.getNumSiren());
+            if (!dto.getPrenom().isEmpty() || dto.getPrenom() != null) {
+                u.setPrenom(dto.getPrenom());
+            }
+            if (!dto.getNom().isEmpty() || dto.getNom() != null) {
+                u.setNom(dto.getNom());
+            }
+            if (!dto.getEmail().isEmpty() || dto.getEmail() != null) {
+                if (control.isEmpty()) {
+                    u.setEmail(dto.getEmail());
+                }else if (control.get().getEmail().equals(u.getEmail())){
+                    u.setEmail(dto.getEmail());
+                }else {
+                    throw new UtilisateurExistantException("email deja utilise");
+                }
+            }
+            if (dto.getNumRue() != 0 || dto.getNumRue() != null) {
+                u.setNumRue(dto.getNumRue());
+            }
+            if (!dto.getRue().isEmpty() || dto.getRue() != null) {
+                u.setRue(dto.getRue());
+            }
+            if (!dto.getVille().isEmpty() || dto.getVille() != null) {
+                u.setVille(dto.getVille());
+            }
+            if (dto.getCodePostal() != 0 || dto.getCodePostal() != null) {
+                u.setCodePostal(dto.getCodePostal());
+            }
+            if (!dto.getNumSiren().isEmpty() || dto.getNumSiren() != null) {
+                u.setNumSiren(dto.getNumSiren());
+            }
             return uDao.save(u);
         }else {
             throw new UtilisateurExistantException("Utilisateur inexistant");
@@ -39,6 +63,9 @@ public class GestionCompteService {
         var user = uDao.findById(dto.getId());
         if (user.isPresent()) {
             UtilisateurEntity u = uDao.findById(dto.getId()).get();
+            if(encoder.matches(dto.getNew_mdp(), u.getMdp())){
+                throw new OldPasswordException("mot de passe deja utilise");
+            }
             u.setMdp(encoder.encode(dto.getNew_mdp()));
             return uDao.save(u);
         } else {
