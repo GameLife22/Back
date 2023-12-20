120b4617939ebf9c0ff89a5e28b957fefe37df43
Quentin Bossuet <qbossuet@gmail.com>
2023-12-20 16:14:28 +0100

    feat : finalisation de la migration postgreSQL

--- gamelife/src/test/java/fr/sqli/formation/gamelife/service/GestionCompteServiceTest.java
+++ gamelife/src/test/java/fr/sqli/formation/gamelife/service/GestionCompteServiceTest.java
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @Rollback
 @Transactional
-@ActiveProfiles("application-test")
+@ActiveProfiles("test")
 class GestionCompteServiceTest {
     @Autowired
     GestionCompteService service;
