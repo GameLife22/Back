0c8499d7f89f4c2a159a7754b1f26c1192776f4f
fabien bidault <fabienbidault@outlook.fr>
2024-03-11 15:12:50 +0100

    [GL-61-Gestion-des-produits] change admin password

--- gamelife/src/main/resources/data.sql
+++ gamelife/src/main/resources/data.sql
@@ -2,7 +2,7 @@
 -- Insertion dans la table glutilisateur
 -- -----------------------------------------------------
 INSERT INTO glutilisateur (nom, prenom, mdp, email, num_rue, rue, ville, code_postal, role, num_siren, etat_compte, reset_password_token)
-VALUES ('admin1', 'admin1', '$2y$10$cllVSeF0YfWJqP69iZ0QHObAqWgj0teCZRmCjXVFSvIDbKpxb4e4u', 'admin1@gamelife.fr', '2', 'rue de capucine', 'paris', '75000', 'ROLE_ADMIN', null, true, null);
+VALUES ('admin1', 'admin1', '$2a$12$v/m/wMNGjDCB52Aecy0xTOpSbk2nGGKTw75m857/fYTS3GCG0ASha', 'admin1@gamelife.fr', '2', 'rue de capucine', 'paris', '75000', 'ROLE_ADMIN', null, true, null);
 INSERT INTO glutilisateur (nom, prenom, mdp, email, num_rue, rue, ville, code_postal, role, num_siren, etat_compte, reset_password_token)
 VALUES ('smith', 'albert', '$2y$10$Be8wPp9hlL8MTMAzpGddt.LDhjww9HQq9WgxVqmrL8wXduVKIYuZO', 'revendeur1@gamelife.fr', '2', 'rue du marechal', 'nantes', '44000', 'ROLE_REVENDEUR', '325987418', true, null);
 INSERT INTO glutilisateur (nom, prenom, mdp, email, num_rue, rue, ville, code_postal, role, num_siren, etat_compte, reset_password_token)
