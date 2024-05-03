94c249b232e6979ecc1643a0ac6ada7936e34678
fabien bidault <fabienbidault@outlook.fr>
2024-05-03 16:38:02 +0200

    déblocage de theesan

--- gamelife/src/main/java/fr/sqli/formation/gamelife/controleur/ProduitControleur.java
+++ gamelife/src/main/java/fr/sqli/formation/gamelife/controleur/ProduitControleur.java
@@ -92,7 +92,7 @@ public class ProduitControleur {
 
     //todo: à supprimer quand yassine aura terminé sa feature
     @GetMapping("/produitrevendeurs/{pProduitId}")
-    public ResponseEntity<List<ProduitRevendeurReponse>> recupererProduitRevendeurs(@PathVariable UUID pProduitId) {
+    public ResponseEntity<List<ProduitRevendeurReponse>> recupererProduitRevendeursParProduit(@PathVariable UUID pProduitId) {
         this.produitRevendeurService.recupererProduitRevendeursParProduit(pProduitId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
